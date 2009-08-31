c36ed3d6b88ffa938374695d28ee2a137c2050a3
Isaac Ezer <isaac.ezer@gmail.com>
2009-08-31 22:05:05 +0900

    adding tests

--- test/unit/helpers/lists_helper_test.rb
+++ test/unit/helpers/lists_helper_test.rb
@@ -1,4 +1,27 @@
 require 'test_helper'
 
 class ListsHelperTest < ActionView::TestCase
+
+ 
+ def test_empty_list
+    @list = List.find( :first, 
+                       :conditions => { :name =>  "emptylist",
+                                        :owner => "isaac" } )
+    assert @list
+    assert @list.items.size == 0   
+  end
+  
+  def test_dm_add
+    assert false == parseTweet( "isaacezer", "books The Tipping Point", "2009-08-01" )
+    @list = List.find( :first, 
+                       :conditions => { :name =>  "books",
+                                        :owner => "isaacezer" } )
+    assert @list
+    
+    @item = @list.items.find( :first,
+                       :conditions => { :author => "isaacezer",
+                                        :fullMessage   => "books The Tipping Point" } ) 
+   assert @item
+ end
+ 
 end
--- test/unit/list_test.rb
+++ test/unit/list_test.rb
@@ -5,4 +5,5 @@ class ListTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
 end
