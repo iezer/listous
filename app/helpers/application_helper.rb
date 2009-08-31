13b2d88485d3956fda24cb5180e9211ba18861fb
Isaac Ezer <isaac.ezer@gmail.com>
2009-08-31 19:55:54 +0900

    fixing page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def title(phrase, container=nil)
+    @page_title = phrase
+    content_tag(container, phrase) if container
+  end
+
 end
