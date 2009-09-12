63a3872d47306bce53104b71239704a8fa717dc1
Isaac Ezer <isaac.ezer@gmail.com>
2009-09-13 00:26:07 +0900

    Adding better error handling. IF we get an error (probably due to twitter rate limiting) wait 30 minutes for next poll.

--- twitter/listous.rb
+++ twitter/listous.rb
@@ -6,12 +6,13 @@ while 1
   success = true
   date = Time.now
   puts "Starting loop " + date.to_s
+  backoff_minutes = 2
   begin
     puts "Polling Twitter for dm and replies to @listous"
     pollTwitter()
   rescue
     puts "error in pollTwitter() sending dm."
-    send_dm( "isaacezer", "Error during Listous loop pollTwitter() " + date.to_s )
+    #send_dm( "isaacezer", "Error during Listous loop pollTwitter() " + date.to_s )
     success = false
   end
   
@@ -20,16 +21,19 @@ while 1
     get_all_users_mentions()
   rescue
     puts "Error polling users' mentions"
-    send_dm( "isaacezer", "Error during Listous loop get_all_users_mentions() " + date.to_s )
+    #send_dm( "isaacezer", "Error during Listous loop get_all_users_mentions() " + date.to_s )
     success = false
   end
   
   if success
     if last_report_day != date.day
-      send_dm( "isaacezer", "Listous loop working fine " + date.to_s )
+      puts ("Success")
+      #send_dm( "isaacezer", "Listous loop working fine " + date.to_s )
       last_report_day = date.day
     end
+  else
+    backoff_minutes = 30
   end
-  sleep 2 * 60
   
+  sleep backoff_minutes * 60
 end
